34782679754bda641460245708404e12df7d7be2
kojiro0208 <h.wadsworth02@gmail.com>
2023-11-22 13:23:44 +0900

    dimentionの修正

--- models/intermediate/int_daily_weathers.sql
+++ models/intermediate/int_daily_weathers.sql
@@ -8,6 +8,12 @@ fin as (
     select
         *,
         case 
+            when average_temperature < 10 then '10度以下'
+            when average_temperature < 20 then '10-19度'
+            when average_temperature < 25 then '20-24度'
+            when average_temperature >= 25 then '25度以上'
+        end as average_temperature_level,
+        case 
             when precipitation >= 50 then '50mm以上'
             when precipitation >= 10 then '10-49mm'
             when precipitation >= 1 then '1-9mm'
--- models/marts/fct_analysis_crimes.sql
+++ models/marts/fct_analysis_crimes.sql
@@ -28,10 +28,33 @@ daily_weather as (
 ),
 _join as (
     select
-        c.*,
+        c.tokyo_crimes_id,
+        c.teguchi,
+        c.city,
+        c.victim_sex,
+        c.victim_age,
+        c.occurrence_time,
+        c.occurrence_date,
+        c.occurrence_month,
+        case
+            when c.occurrence_time_num is null then null
+            when c.occurrence_time_num < 2 then '00-02時'
+            when c.occurrence_time_num < 4 then '02-04時'
+            when c.occurrence_time_num < 6 then '04-06時'
+            when c.occurrence_time_num < 8 then '06-08時'
+            when c.occurrence_time_num < 10 then '08-10時'
+            when c.occurrence_time_num < 12 then '10-12時'
+            when c.occurrence_time_num < 14 then '12-14時'
+            when c.occurrence_time_num < 16 then '14-16時'
+            when c.occurrence_time_num < 18 then '16-18時'
+            when c.occurrence_time_num < 20 then '18-20時'
+            when c.occurrence_time_num < 22 then '20-22時'
+            when c.occurrence_time_num < 24 then '22-24時'
+        end as occurrence_time_str,
         cm.longitude,
         cm.latitude,
         dw.day_of_week,
+        dw.average_temperature_level,
         dw.precipitation_level
     from
         crime as c
