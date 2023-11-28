2a2370766e7c4e8075bfb27a3b305538db9b3ae3
kojiro0208 <h.wadsworth02@gmail.com>
2023-11-28 15:47:25 +0900

    処理層の変更

--- models/intermediate/int_tokyo_crimes.sql
+++ models/intermediate/int_tokyo_crimes.sql
@@ -24,7 +24,22 @@ add_columns as (
 fin as (
     select
         *,
-        {{ kanji_to_num("occurrence_address") }} as occurrence_address_num
+        {{ kanji_to_num("occurrence_address") }} as occurrence_address_num,
+        case
+            when occurrence_time_num is null then null
+            when occurrence_time_num < 2 then '00-02時'
+            when occurrence_time_num < 4 then '02-04時'
+            when occurrence_time_num < 6 then '04-06時'
+            when occurrence_time_num < 8 then '06-08時'
+            when occurrence_time_num < 10 then '08-10時'
+            when occurrence_time_num < 12 then '10-12時'
+            when occurrence_time_num < 14 then '12-14時'
+            when occurrence_time_num < 16 then '14-16時'
+            when occurrence_time_num < 18 then '16-18時'
+            when occurrence_time_num < 20 then '18-20時'
+            when occurrence_time_num < 22 then '20-22時'
+            when occurrence_time_num < 24 then '22-24時'
+        end as occurrence_time_str,
     from
         add_columns
 )
--- models/marts/fct_analysis_crimes.sql
+++ models/marts/fct_analysis_crimes.sql
@@ -36,21 +36,6 @@ _join as (
         c.occurrence_time,
         c.occurrence_date,
         c.occurrence_month,
-        case
-            when c.occurrence_time_num is null then null
-            when c.occurrence_time_num < 2 then '00-02時'
-            when c.occurrence_time_num < 4 then '02-04時'
-            when c.occurrence_time_num < 6 then '04-06時'
-            when c.occurrence_time_num < 8 then '06-08時'
-            when c.occurrence_time_num < 10 then '08-10時'
-            when c.occurrence_time_num < 12 then '10-12時'
-            when c.occurrence_time_num < 14 then '12-14時'
-            when c.occurrence_time_num < 16 then '14-16時'
-            when c.occurrence_time_num < 18 then '16-18時'
-            when c.occurrence_time_num < 20 then '18-20時'
-            when c.occurrence_time_num < 22 then '20-22時'
-            when c.occurrence_time_num < 24 then '22-24時'
-        end as occurrence_time_str,
         cm.longitude,
         cm.latitude,
         dw.day_of_week,
